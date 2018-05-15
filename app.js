96c2ccbf1d064ee7c504f43acb6c9bdc713da0a4
Jeff <jeff@JeffdeMBP.Home>
2018-05-15 22:44:54 +0800

    add gulp file

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+	res.send('Welcome to my API!');
+});
+
+app.listen(port, function () {
+	console.log('Running on port: ' + port);
+});
