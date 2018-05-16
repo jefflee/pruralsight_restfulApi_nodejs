9895467b9c5ffbac01e0f1529f2537f5ef9aa5ab
Jeff <jeff@JeffdeMBP.Home>
2018-05-16 23:48:57 +0800

    move logic to controller Add test cases

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-var db = mongoose.connect('mongodb://localhost/bookAPI');
+var db;
+console.log('Hello');
+if (process.env.ENV == 'Test') {
+
+	db = mongoose.connect('mongodb://localhost/bookAPI_test');
+}
+
+else {
+	db = mongoose.connect('mongodb://localhost/bookAPI');
+}
 
 var Book = require('./models/bookModel');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 bookRouter = require('./Routes/bookRoutes')(Book);
@@ -18,9 +27,11 @@ bookRouter = require('./Routes/bookRoutes')(Book);
 app.use('/api/books', bookRouter);
 
 app.get('/', function (req, res) {
-	res.send('Welcome to my API!');
+	res.send('Welcome to my API!test ');
 });
 
 app.listen(port, function () {
 	console.log('Gulp is running my app on port: ' + port);
 });
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var gulpMocha = require('gulp-mocha');
+var env = require('gulp-env');
+var supertest = require('supertest');
 
-
-gulp.task('default', function(){
+gulp.task('default', function () {
     nodemon({
         script: 'app.js',
         ext: 'js',
         env: {
-            PORT:8000
+            PORT: 8000
         },
         ignore: ['./node_modules/**']
     })
-    .on('restart', function(){
-        console.log('Restarting');
-    });
+        .on('restart', function () {
+            console.log('Restarting');
+        });
+});
+
+gulp.task('test', function () {
+    env({ vars: { ENV: 'Test' } });
+    gulp.src('tests/*.js', { read: false })
+        .pipe(gulpMocha({ reporter: 'nyan' }))
 });
